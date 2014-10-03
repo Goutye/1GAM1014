20312ffeb8fd7819506ee6215780a2da7fdb1840
F4r3n <guillaume_k@hotmail.fr>
2014-10-03 21:41:51 +0200

    add options menu

---
+++ src/ogam1014/screen/MOptions.java
@@ -0,0 +1,64 @@
+package ogam1014.screen;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import ogam1014.Engine;
+
+
+
+public class MOptions extends Screen{
+	private Button back = new Button((int) Engine.WIDTH -100,
+			(int) (Engine.HEIGHT * 0.8), 100, 50, "Back", Color.BLACK,
+			Color.YELLOW);
+	
+	private Button sound = new Button((int) Engine.WIDTH/5,
+			(int) (Engine.HEIGHT * 0.2), 100, 50, "Sound", Color.BLACK,
+			Color.YELLOW);
+	private List<Button> buttons = new ArrayList<Button>();
+	private int counter = 0;
+	
+	public MOptions(){
+		
+		buttons.add(sound);
+		buttons.add(back);
+		
+	}
+	
+	public void update(double dt){
+		if (input.validate.down && counter == 0) {
+			
+		}
+		if (input.validate.pressed && counter == 1) {
+			engine.setScreen(new Menu());
+		}
+		if (input.up.pressed) {
+			if (counter > 0)
+				counter--;
+
+		}
+		if (input.down.pressed) {
+			counter++;
+
+		}
+
+		counter %= buttons.size();
+	}
+	
+	
+	public void draw(Graphics g){
+		int i = 0;
+		for (Button button : buttons) {
+			if (counter == i) {
+				button.drawSelectedRectangle(g);
+			} else
+				button.drawRectangle(g);
+			i++;
+
+		}
+
+	}
+
+}
--- src/ogam1014/screen/Menu.java
+++ src/ogam1014/screen/Menu.java
@@ -8,55 +8,61 @@ import java.util.List;
 import ogam1014.Engine;
 
 public class Menu extends Screen {
-	
-	private Button start = new Button((int)Engine.WIDTH / 2, (int) (Engine.HEIGHT * 0.2),100,50,"Start",Color.BLACK,Color.YELLOW);
-	private Button options = new Button((int)Engine.WIDTH / 2, (int) (Engine.HEIGHT * 0.4),100,50,"Options",Color.BLACK,Color.YELLOW);
-	private Button quit = new Button((int)Engine.WIDTH / 2, (int) (Engine.HEIGHT * 0.6),100,50,"Quit",Color.BLACK,Color.YELLOW);
+
+	private Button start = new Button((int) Engine.WIDTH / 2,
+			(int) (Engine.HEIGHT * 0.2), 100, 50, "Start", Color.BLACK,
+			Color.YELLOW);
+	private Button options = new Button((int) Engine.WIDTH / 2,
+			(int) (Engine.HEIGHT * 0.4), 100, 50, "Options", Color.BLACK,
+			Color.YELLOW);
+	private Button quit = new Button((int) Engine.WIDTH / 2,
+			(int) (Engine.HEIGHT * 0.6), 100, 50, "Quit", Color.BLACK,
+			Color.YELLOW);
 	private List<Button> buttons = new ArrayList<Button>();
-	private int counter=0;
+	private int counter = 0;
+
 	
-	
-	public Menu(){
+
+	public Menu() {
 		buttons.add(start);
 		buttons.add(options);
 		buttons.add(quit);
-		
+
 	}
-	
-	
+
 	public void update(double dt) {
-		
-		if (input.validate.down) {
+
+		if (input.validate.pressed && counter == 0) {
 			engine.setScreen(new Game());
 		}
-		if(input.up.pressed){
-			if(counter>0)
-			counter--;
-	
-			
+		if (input.validate.pressed && counter == 1) {
+			engine.setScreen(new MOptions());
 		}
-		if(input.down.pressed){
+		if (input.up.pressed) {
+			if (counter > 0)
+				counter--;
+
+		}
+		if (input.down.pressed) {
 			counter++;
-			
+
 		}
-		
-		counter%=buttons.size();
+
+		counter %= buttons.size();
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		int i=0;
-		for(Button button:buttons){
-			if(counter==i){
+		int i = 0;
+		for (Button button : buttons) {
+			if (counter == i) {
 				button.drawSelectedRectangle(g);
-			}
-			else
-			button.drawRectangle(g);
+			} else
+				button.drawRectangle(g);
 			i++;
-			
+
 		}
-	
-		//g.drawString(Engine.NAME, (int)Engine.WIDTH / 2, (int) (Engine.HEIGHT * 0.2));
+
 	}
 
 }
