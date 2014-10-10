25e9d4a0e5d3303c212c02241fccd27e576cda28
Steven Le Rouzic <steven.lerouzic@gmail.com>
2014-10-10 21:38:17 +0200

    player inventory

--- src/ogam1014/entity/Player.java
+++ src/ogam1014/entity/Player.java
@@ -1,6 +1,7 @@
 package ogam1014.entity;
 
 import ogam1014.InputHandler;
+import ogam1014.equipment.PlayerInventory;
 import ogam1014.graphics.Renderer;
 
 public class Player extends LivingEntity {
@@ -10,11 +11,14 @@ public class Player extends LivingEntity {
 	private int w;
 	private int h;
 	
+	private PlayerInventory inventory;
 	
 	public Player(InputHandler input) {
 		this.input = input;
 		this.w = 22;
 		this.h = 43;
+		
+		inventory = new PlayerInventory(this);
 	}
 
 	@Override
@@ -64,7 +68,8 @@ public class Player extends LivingEntity {
 			level.addEntity(b);
 		}
 
-		super.update(dt);
+		super.update(dt);		
+		inventory.update(dt);
 	}
 
 	@Override
---
+++ src/ogam1014/equipment/PlayerInventory.java
@@ -0,0 +1,55 @@
+package ogam1014.equipment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ogam1014.entity.Player;
+
+public class PlayerInventory {
+	
+	private Player player;
+	private List<Item> items = new ArrayList<Item>();
+	private Map<ArmorType, ArmorItem> armor = new HashMap<ArmorType, ArmorItem>();
+	private IUsableItem[] equipped = new IUsableItem[3];
+	
+	public PlayerInventory(Player player) {
+		equipped[0] = null;
+		equipped[1] = null;
+		equipped[2] = null;
+		
+		for(ArmorType type : ArmorType.values()) {
+			armor.put(type, null);
+		}
+		
+		this.player = player;
+	}
+	
+	public void update(double dt) {
+		for(Item item : items) {
+			if(item instanceof IUpdatableItem) {
+				((IUpdatableItem) items).update(dt);
+			}
+		}
+	}
+	
+	public void use(int slot) {
+		if(slot < 0 || slot > 2)
+			return;
+		
+		if(equipped[slot] != null) {
+			equipped[slot].use(player);
+		}
+	}
+	
+	public float getTotalArmorProtection() {
+		float total = 0;
+		
+		for(ArmorType type : ArmorType.values()) {
+			total += armor.get(type).getProtection();
+		}
+		
+		return total;
+	}
+}
