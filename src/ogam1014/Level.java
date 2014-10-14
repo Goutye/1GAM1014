35d68afac6ff91857bdd5dea81870a1e5e6f00f6
kidanger <kidanger@gmail.com>
2014-10-14 13:59:09 +0200

    display: zoom

--- src/ogam1014/Level.java
+++ src/ogam1014/Level.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
-
 import ogam1014.entity.Enemy;
 import ogam1014.entity.Entity;
 import ogam1014.entity.Player;
