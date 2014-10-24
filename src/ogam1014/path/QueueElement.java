e77dd4d36e6e5979b34123cb1057a12e9aceb8dc
manumanmax <emmanuelbretonbelz@gmail.com>
2014-10-24 13:11:46 +0200

    addition of the end in the elements

--- src/ogam1014/path/QueueElement.java
+++ src/ogam1014/path/QueueElement.java
@@ -2,13 +2,17 @@ package ogam1014.path;
 
 import java.awt.Point;
 
-public class QueueElement {
+import com.sun.javafx.collections.SortableList;
+
+public class QueueElement implements Comparable {
 	private Point tile;
 	private int precedente;
+	private Point end;
  
-	public QueueElement(Point tile) {
+	public QueueElement(Point tile, Point end) {
 		this.tile = tile;
 		this.precedente = 0;
+		this.end = end;
 	}
 	
 	public QueueElement(Point tile, int precedente) {
@@ -21,5 +25,15 @@ public class QueueElement {
 	}
 	public int getPrecedente() {
 		return precedente;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if(o instanceof QueueElement){
+			QueueElement qe = (QueueElement) o;
+			
+			
+		}
+		return 0;
 	}
 }
