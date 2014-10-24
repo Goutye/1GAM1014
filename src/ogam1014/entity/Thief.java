2d070da7c5cbf7d703e409585dbdffa365605b9e
Aomeas <aomeas@gmail.com>
2014-10-24 13:55:17 +0200

    Added player clone

--- src/ogam1014/entity/Thief.java
+++ src/ogam1014/entity/Thief.java
@@ -3,6 +3,7 @@ package ogam1014.entity;
 import java.awt.Point;
 
 import ogam1014.equipment.ItemFactory;
+import ogam1014.graphics.Renderer;
 
 public class Thief extends Enemy {
 	private static final long serialVersionUID = 8877449123438883245L;
@@ -49,8 +50,100 @@ public class Thief extends Enemy {
 			dx = 0;
 			dy = 0;
 		}
+		if (dy == 0){
+			if (dx > 0){
+				dir = Direction.RIGHT;
+			} else if (dx < 0){
+				dir = Direction.LEFT;
+			} else {
+				dir = Direction.DOWN;
+			}
+		} else {
+			if (dy > 0){
+				dir = Direction.DOWN;
+			} else {
+				dir = Direction.UP;
+			}
+		}
 	}
 
+	@Override
+	public void draw(Renderer r) {
+		if (Math.abs(dx) > 0 || Math.abs(dy) > 0 ){
+			if (dir == Direction.UP){
+				int frameNumber = (int)(Math.ceil(time*8) % 6);
+				if (frameNumber == 37){
+					r.blit(IMAGE, x, y, w, h, 120, 37);
+				} else if (frameNumber == 1){
+					r.blit(IMAGE, x, y, w, h, 135, 37);
+				} else if (frameNumber == 2){
+					r.blit(IMAGE, x, y, w, h, 150, 37);
+				} else if (frameNumber == 3){
+					r.blit(IMAGE, x, y, w, h, 165, 37);
+				} else if (frameNumber == 4){
+					r.blit(IMAGE, x, y, w, h, 150, 37);
+				} else {
+					r.blit(IMAGE, x, y, w, h, 135, 37);
+				}
+			} else if (dir == Direction.LEFT) {
+				int frameNumber = (int)(Math.ceil(time*8) % 6);
+				if (frameNumber == 37){
+					r.blit(IMAGE, x, y, w, h, 180, 37);
+				} else if (frameNumber == 1){
+					r.blit(IMAGE, x, y, w, h, 195, 37);
+				} else if (frameNumber == 2){
+					r.blit(IMAGE, x, y, w, h, 210, 37);
+				} else if (frameNumber == 3){
+					r.blit(IMAGE, x, y, w, h, 225, 37);
+				} else if (frameNumber == 4){
+					r.blit(IMAGE, x, y, w, h, 210, 37);
+				} else {
+					r.blit(IMAGE, x, y, w, h, 195, 37);
+				}
+			} else if (dir == Direction.RIGHT) {	
+				int frameNumber = (int)(Math.ceil(time*8) % 6);
+				if (frameNumber == 37){
+					r.blit(IMAGE, x, y, w, h, 240, 37);
+				} else if (frameNumber == 1){
+					r.blit(IMAGE, x, y, w, h, 255, 37);
+				} else if (frameNumber == 2){
+					r.blit(IMAGE, x, y, w, h, 270, 37);
+				} else if (frameNumber == 3){
+					r.blit(IMAGE, x, y, w, h, 285, 37);
+				} else if (frameNumber == 4){
+					r.blit(IMAGE, x, y, w, h, 270, 37);
+				} else {
+					r.blit(IMAGE, x, y, w, h, 255, 37);
+				}
+			} else {
+				int frameNumber = (int)(Math.ceil(time*8) % 6);
+				if (frameNumber == 37){
+					r.blit(IMAGE, x, y, w, h, 60, 37);
+				} else if (frameNumber == 1){
+					r.blit(IMAGE, x, y, w, h, 75, 37);
+				} else if (frameNumber == 2){
+					r.blit(IMAGE, x, y, w, h, 90, 37);
+				} else if (frameNumber == 3){
+					r.blit(IMAGE, x, y, w, h, 105, 37);
+				} else if (frameNumber == 4){
+					r.blit(IMAGE, x, y, w, h, 90, 37);
+				} else {
+					r.blit(IMAGE, x, y, w, h, 75, 37);
+				}
+			}
+		} else {
+			if (dir == Direction.UP){
+				r.blit(IMAGE, x, y, w, h, 15, 37);
+			} else if (dir == Direction.LEFT) {
+				r.blit(IMAGE, x, y, w, h, 30, 37);
+			} else if (dir == Direction.RIGHT) {	
+				r.blit(IMAGE, x, y, w, h, 45, 37);
+			} else {
+				r.blit(IMAGE, x, y, w, h, 0, 37);
+			}
+		}
+	}
+	
 	@Override
 	protected void onDeath() {
 		super.onDeath();
